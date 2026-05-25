e36e2135b6574ba60026807c675ff1092f6912dc
Pastinioo <pastinio02@wp.pl>
2026-05-25 09:34:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Wybierz kierunek konwersji (C -> na Fahrenheity, F -> na Celsjusze): ");
+        string wybor = Console.ReadLine().ToUpper();
+
+        Console.Write("Podaj wartość temperatury: ");
+        double temp = Convert.ToDouble(Console.ReadLine());
+
+        if (wybor == "C")
+        {
+            double f = temp * 1.8 + 32;
+            Console.WriteLine($"{temp}°C = {f:F2}°F");
+        }
+        else if (wybor == "F")
+        {
+            double c = (temp - 32) / 1.8;
+            Console.WriteLine($"{temp}°F = {c:F2}°C");
+        }
+        else
+        {
+            Console.WriteLine("Nieprawidłowy wybór kierunku konwersji.");
+        }
+    }
+}
