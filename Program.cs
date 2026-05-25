6f3556f6b511fced7666bed80a907770ba9c9e45
Pastinioo <pastinio02@wp.pl>
2026-05-25 09:28:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj pierwszą liczbę: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj drugą liczbę: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Wybierz operację (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        if (op == "+")
+        {
+            Console.WriteLine($"Wynik: {num1 + num2}");
+        }
+        else if (op == "-")
+        {
+            Console.WriteLine($"Wynik: {num1 - num2}");
+        }
+        else if (op == "*")
+        {
+            Console.WriteLine($"Wynik: {num1 * num2}");
+        }
+        else if (op == "/")
+        {
+            if (num2 != 0)
+            {
+                Console.WriteLine($"Wynik: {num1 / num2}");
+            }
+            else
+            {
+                Console.WriteLine("Błąd: Nie można dzielić przez zero!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nieznana operacja.");
+        }
+    }
+}
